469ee2f9a56007888e8189970c1781ec282e116e
Pavel <krasulinpavel1@yandex.ru>
2023-02-23 22:04:14 +0400

    Add app styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import TodoList from "./Todo/TodoList";
 
 function App() {
-
   const todoData = [
     {id: 1, complited: false, title: 'Купить хлеб'},
     {id: 2, complited: false, title: 'Купить картофель'},
@@ -10,8 +9,10 @@ function App() {
 
   return (
     <div className="wrapper">
-      <h1>To-Do List</h1>
-      <TodoList todos={todoData} />
+      <div className="container">
+        <h1 className="title-h1">To-Do List</h1>
+        <TodoList todos={todoData} />
+      </div>
     </div>
   );
 }
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,9 +1,28 @@
 export default function TodoItem({ todo, index }) {
+  const styles = {
+    li: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      border: '3px solid transparent',
+      borderRadius: '8px',
+      marginBottom: '5px',
+      padding: '10px',
+      transition: 'background-color .2s cubic-bezier(.32,.08,.24,1)',
+    },
+    input: {
+      marginRight: '10px',
+    },
+    strong: {
+      marginRight: '5px',
+    },
+  };
+
   return (
-    <li>
+    <li style={styles.li}>
       <span>
-        <input type="checkbox" />
-        <strong>
+        <input type="checkbox" style={styles.input} />
+        <strong style={styles.strong}>
           {index + 1}
         </strong>
         {todo.title}

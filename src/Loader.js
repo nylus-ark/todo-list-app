f3ff792c496173bbb4b7e52bfea677ad97ddeb9a
nylus-ark <nylus-ark@users.noreply.github.com>
2023-03-10 10:55:42 +0000

    deploy: 69c0c8c75555c2240d11e62d6a590c065c0e3ef2

--- src/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => (
-    <div style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>
-        <div className="lds-dual-ring" />
-    </div>
-)
